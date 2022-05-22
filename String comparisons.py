e7ccc23e5ab0a00963a0fdb42cf734acb13c253a
Mallory-Mueller <mallory.m.mueller@gmail.com>
2022-05-21 23:08:25 -0500

    Beginner Projects continued

---
+++ String comparisons.py
@@ -0,0 +1,9 @@
+frequency=" once a week"
+intensity= "low"
+highly_active= frequency=="daily"
+print("highly active use:")
+print(highly_active)
+high_intensity=intensity=="high"
+print("High intensity sports:")
+print (high_intensity)
+
