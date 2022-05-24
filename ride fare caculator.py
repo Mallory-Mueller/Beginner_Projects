c025d0cd2384fc4e1037d740627d0f4bcc6e099e
Mallory-Mueller <mallory.m.mueller@gmail.com>
2022-05-24 13:00:40 -0500

    Beginner Projects continued

---
+++ ride fare caculator.py
@@ -0,0 +1,20 @@
+ride_type= "black"
+credits=4
+
+ride_price=0
+final_price=0
+
+if ride_type=="ridex":
+    ride_price=23.5
+elif ride_type=="rideblack":
+    ride_price=32
+else: ride_price=17.5
+
+Print("Ride price:)
+Print (ride_price)
+
+if credits > 0:
+    final_price= ride_price-credits
+
+print ("Final price:")
+print (final_price)
